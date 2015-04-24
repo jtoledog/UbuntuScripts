eb6fed2c51e1575146ed9df113e1b66b8a5f4a1e
Jaco Toledo Gerrish <jaco@jgerrish.com>
2015-04-24 14:04:36 -0400

    Update grabsend.sh

--- Security/grabsend/grabsend.sh
+++ Security/grabsend/grabsend.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 # date
 ts=`date`
+# temp dir
+TDIR=`mktemp -d`
+#trap to remove temp dir
+trap "{ cd - ; rm -rf $TDIR; exit 255; }" SIGINT
+
+cd $TDIR
 #Takes 3 Pictures and saves them to /tmp
-avconv -f video4linux2 -i /dev/video0 -vframes 3 /tmp/image%01d.jpg
+avconv -f video4linux2 -i /dev/video0 -vframes 3 image%01d.jpg >/dev/null 2>/dev/null
 #Stich Images
-$(montage -geometry +4+4 -compress none -quality 100 /tmp/image1.jpg /tmp/image2.jpg /tmp/image3.jpg /tmp/result.jpg)
-#Sends The Pictures to user@email.com
-mpack -s "Auth Failure $ts" /tmp/result.jpg user@email.com
-#Removes The Pictures
-rm /tmp/image1.jpg
-rm /tmp/image2.jpg
-rm /tmp/image3.jpg
-rm /tmp/result.jpg
-exit 0  
+$(montage -geometry +4+4 -compress none -quality 100 image1.jpg image2.jpg image3.jpg result.jpg)
+#Sends The Pictures
+mpack -s "Auth Failure $ts" result.jpg user@email.com
+cd - >/dev/null 2>/dev/null
+
+# Remove Temp Files
+rm -rf $TDIR
+
+exit 0
 #important for pam not to stall - has to exit with status 0
